84e04971ebb7f166edd62269e21a799f72795ded
ultraniu8m <95349360+ultraniu8m@users.noreply.github.com>
2021-12-08 16:53:14 +0300

    Add files via upload

---
+++ 08.12_oaip/08.12_oaip/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _08._12_oaip
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Click += button1_Click;
+        }
+        private void button1_Click(object? sender, EventArgs e)
+        {
+            MessageBox.Show("Привет");
+        }
+    }
+}
